64af5bf6ad96ffe1aa66661482460308d5a962e8
Tiiseto Ramahole <radiopanetiisetso@gmail.com>
2024-02-29 07:06:59 +0200

    Completed Chapter 02

--- Chapter02/Exercise_2.2.js
+++ Chapter02/Exercise_2.2.js
@@ -1,6 +1,6 @@
 const myName = "Tiisetso";
 const myAge = 100;
 const coder = true;
-const message = `HEllo, I'm ${myName}, Years: ${myAge}, I can code Javascript: ${coder}`
+const message = `Hello, I'm ${myName}, Years: ${myAge}, I can code Javascript: ${coder}`
 
 console.log(message);
--- Chapter02/Exercise_2.4.js
+++ Chapter02/Exercise_2.4.js
@@ -2,7 +2,7 @@ let a = 4;
 let b = 56;
 let c = 30;
 
-a = a + b;
-a = a / c;
-c = c % b;
+a = a + b; // 
+a = a / c; // 
+c = c % b; // % remainder
 console.log(a, b, c);
